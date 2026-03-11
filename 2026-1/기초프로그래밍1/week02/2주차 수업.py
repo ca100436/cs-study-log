297737898b99db9e2a86ec0364aaede6d645e776
Yu Sungyoon <ca100436@gmail.com>
2026-03-11 12:25:43 +0900

    BOJ 11720 숫자의 합 문제 풀이

---
+++ 2026-1/기초프로그래밍1/week02/2주차 수업.py
@@ -0,0 +1,19 @@
+import turtle as t
+t.shape("turtle")
+'''
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+'''
+'''
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+'''
+t.color("red")
